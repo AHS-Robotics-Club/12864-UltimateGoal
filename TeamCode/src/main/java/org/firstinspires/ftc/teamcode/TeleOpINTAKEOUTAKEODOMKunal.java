9e23beae33372398830bf8e487cafb127aa0c572
Jackson Isenberg <jaxonfiles@gatech.edu>
2020-10-20 17:40:34 -0400

    Fix package statement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpINTAKEOUTAKEODOMKunal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpINTAKEOUTAKEODOMKunal.java
@@ -1,5 +1,4 @@
-package org.firstinspires.ftc.teamcode.TeleOp;
-
+package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
