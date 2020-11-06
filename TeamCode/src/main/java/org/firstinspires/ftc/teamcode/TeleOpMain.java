423a31edd414d1668886425804488fa78d98b867
liteheded <57595571+liteheded@users.noreply.github.com>
2020-11-06 16:15:43 -0500

    I fixed stuff
    Done I fixed everything I think here you go

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
 
+import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.button.Button;
 import com.arcrobotics.ftclib.command.button.GamepadButton;
 import com.arcrobotics.ftclib.gamepad.ButtonReader;
@@ -17,13 +18,12 @@ import org.firstinspires.ftc.teamcode.subsystems.DriveSystem;
 import org.firstinspires.ftc.teamcode.subsystems.ShooterSystem;
 import org.firstinspires.ftc.teamcode.subsystems.commands.Com_Drive;
 import org.firstinspires.ftc.teamcode.subsystems.commands.Com_NoShoot;
-import org.firstinspires.ftc.teamcode.subsystems.commands.Com_SetShootPower;
 import org.firstinspires.ftc.teamcode.subsystems.commands.Com_Shoot;
 
 @TeleOp(name = "CommandBaseTest")
 public class TeleOpMain extends CommandOpMode {
 
-    public int pwrSelect = 0;
+    public double pwrSelect;
 
     private Motor fL, bL, fR, bR;
     private MotorEx shot;
@@ -33,11 +33,9 @@ public class TeleOpMain extends CommandOpMode {
     private ShooterSystem shooterSystem;
     private Com_Shoot shootCommand;
     private Com_NoShoot stopCommand;
-    private Com_SetShootPower shooterPowerCommand;
 
     public GamepadEx m_driverOp, m_toolOp;
-    private Button shooterStart, shooterStop;
-    private ButtonReader dPadUp, dPadDown;
+    private Button shooterStart, shooterStop, dpadUp, dpadDown;
 
     @Override
     public void initialize() {
@@ -47,32 +45,34 @@ public class TeleOpMain extends CommandOpMode {
         bR = new Motor(hardwareMap, "bR");
 
         shot = new MotorEx(hardwareMap, "shot", Motor.GoBILDA.BARE);
+        shot.setRunMode(Motor.RunMode.VelocityControl);
 
         mecDrive = new DriveSystem(fL, fR, bL, bR);
 
         m_driverOp = new GamepadEx(gamepad1);
         m_toolOp = new GamepadEx(gamepad2);
 
-        //this all is for shooter power adjustment:
-        //the code below is for looping through modes 1-3
-        //after a case statement within the ShooterSystem class checks the mode and sets the motor power
-        dPadUp = new ButtonReader(m_toolOp, GamepadKeys.Button.DPAD_UP);
-        dPadDown = new ButtonReader(m_toolOp, GamepadKeys.Button.DPAD_DOWN);
-
-        if(dPadDown.wasJustPressed()){
-            pwrSelect--;
-            if(pwrSelect <= 0)
-                pwrSelect = 3;
-        }else if(dPadUp.wasJustPressed()){
-            pwrSelect++;
-            if(pwrSelect >= 4)
-                pwrSelect = 1;
-        }
+        dpadDown = new GamepadButton(m_toolOp, GamepadKeys.Button.DPAD_DOWN)
+                .whenPressed(new InstantCommand(() -> {
+                    if (pwrSelect < 0.05) {
+                        pwrSelect = 1;
+                    } else {
+                        pwrSelect -= 0.25;
+                    }
+                }));
+        dpadUp = new GamepadButton(m_toolOp, GamepadKeys.Button.DPAD_UP)
+                .whenPressed(new InstantCommand(() -> {
+                    if (pwrSelect > 0.95) {
+                        pwrSelect = 0;
+                    } else {
+                        pwrSelect += 0.25;
+                    }
+                }));
 
         driveCommand = new Com_Drive(mecDrive, m_driverOp::getLeftX, m_driverOp::getLeftY, m_driverOp::getRightX);
 
         //IMPORTANT: Note to self remember in the Drive System class I just flipped the turn speed and strafe speed
-        shooterSystem = new ShooterSystem(shot, this);
+        shooterSystem = new ShooterSystem(shot, telemetry, () -> pwrSelect);
         shootCommand = new Com_Shoot(shooterSystem);
         stopCommand = new Com_NoShoot(shooterSystem);
 
@@ -85,7 +85,7 @@ public class TeleOpMain extends CommandOpMode {
 
         register(mecDrive, shooterSystem);
 
-        schedule(driveCommand, shooterPowerCommand);
+        schedule(driveCommand);
     }
 }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterSystem.java
@@ -5,51 +5,27 @@ import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.TeleOpMain;
 
 import java.util.function.DoubleSupplier;
 
 public class ShooterSystem extends SubsystemBase {
-    private MotorEx Motor_S;
-    private TeleOpMain teleop;
-    public double shooterPower;
+    private MotorEx shooterMotor;
+    private Telemetry telemetry;
+    private DoubleSupplier power;
 
-    public ShooterSystem(MotorEx ShooterMotor, TeleOpMain tele){
-        Motor_S = ShooterMotor;
-        teleop = tele;
-    }
-
-    public void adjust(){
-        //These are just place holder values need real ones
-        switch(teleop.pwrSelect) {
-            case 1:
-                shooterPower = 0.5;
-                teleop.telemetry.addData("Shooter Power:", shooterPower);
-                teleop.telemetry.update();
-                break;
-            case 2:
-                shooterPower = 0.75;
-                teleop.telemetry.addData("Shooter Power:", shooterPower);
-                teleop.telemetry.update();
-                break;
-            case 3:
-                shooterPower = 1;
-                teleop.telemetry.addData("Shooter Power:", shooterPower);
-                teleop.telemetry.update();
-                break;
-            default:
-                shooterPower = 0;
-                teleop.telemetry.addData("Shooter Power:", shooterPower);
-                teleop.telemetry.update();
-                break;
-        }
+    public ShooterSystem(MotorEx ShooterMotor, Telemetry telemetryIn, DoubleSupplier getPower){
+        shooterMotor = ShooterMotor;
+        telemetry = telemetryIn;
+        power = getPower;
     }
 
     public void shoot(){
-        Motor_S.set(shooterPower);
+        shooterMotor.set(power.getAsDouble());
     }
 
     public void stop(){
-        Motor_S.set(0);
+        shooterMotor.set(0);
     }
 }
