e376deb0f5acb8c3008145a22cc52a2e6bdadab4
Rishab Seshadri <rishab.seshadri@gmail.com>
2020-12-05 05:44:55 -0500

    pog? :q git commit -m pog?

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/commands/Com_PickUp.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.subsystems.commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.subsystems.WobbleSystem;
+
+public class Com_PickUp extends CommandBase {
+    private final WobbleSystem wobblySystem;
+    private Motor motor;
+    private ElapsedTime timer;
+    public Com_PickUp(WobbleSystem subby, ElapsedTime time){
+        wobblySystem = subby;
+        motor = wobblySystem.getMotor();
+        motor.setPositionCoefficient(0.01);
+        motor.setPositionTolerance(10);
+        motor.setTargetPosition(350);
+
+        addRequirements(subby);
+    }
+
+    @Override
+    public void initialize(){
+        motor.stopMotor();
+    }
+    @Override
+    public void execute(){
+        wobblySystem.armUp();
+    }
+    @Override
+    public void end(boolean interruptable){
+        wobblySystem.motorStop();
+    }
+    @Override
+    public boolean isFinished(){
+        return motor.atTargetPosition() || timer.seconds() > 2;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/commands/Com_PutDown.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.subsystems.commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.subsystems.WobbleSystem;
+
+public class Com_PutDown extends CommandBase {
+
+    private final WobbleSystem wobblySystem;
+
+    private  Motor motor;
+    private Telemetry telemetry;
+
+    public Com_PutDown(WobbleSystem subby){
+        wobblySystem = subby;
+        motor = wobblySystem.getMotor();
+        motor.setPositionCoefficient(0.01);
+        motor.setPositionTolerance(10);
+        motor.setTargetPosition(-330);
+        telemetry = wobblySystem.getTele();
+        addRequirements(subby);
+    }
+    @Override
+    public void initialize(){
+        motor.stopMotor();
+        telemetry.addData("awdwad", "awdaw");
+        telemetry.update();
+    }
+    @Override
+    public void execute(){
+        wobblySystem.armDown();
+    }
+    @Override
+    public void end(boolean interruptable){
+        wobblySystem.motorStop();
+    }
+    @Override
+    public boolean isFinished(){
+        return motor.atTargetPosition();
+    }
+}
